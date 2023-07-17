8881b7b3fe0a4c4fd269bdf79894c5f9b0f96eb0
Adriano Schneider Maximiano <adriano-sm@hotmail.com>
2023-07-17 22:22:37 +0100

    starting on web design

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="container navbar navbar-light bg-light p-4">
+      <Link to="/">
+        <span className="navbar-brand mb-0 h1">
+          <img src="https://static.wikia.nocookie.net/logopedia/images/2/25/Star_Wars.svg" />
+        </span>
+      </Link>
+      <div className="ml-auto">
+        <div class="dropdown">
+          <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Dropdown
+          </button>
+          <ul class="dropdown-menu">
+            <li>
+              <button class="dropdown-item" type="button">
+                Action
+              </button>
+            </li>
+            <li>
+              <button class="dropdown-item" type="button">
+                Another action
+              </button>
+            </li>
+            <li>
+              <button class="dropdown-item" type="button">
+                Something else here
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
